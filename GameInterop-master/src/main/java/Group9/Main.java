fc400467dfc2713d64bb55e788c7e9d963d7b847
Adrien <adrienigcse0003@gmail.com>
2020-06-12 12:38:58 +0200

    them fixes

--- GameInterop-master/src/main/java/Group9/Main.java
+++ GameInterop-master/src/main/java/Group9/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Game game = new Game(Parser.parseFile("./src/main/java/Group9/map/maps/test_2.map"), new DefaultAgentFactory(), false);
+        Game game = new Game(Parser.parseFile("C:/Users/Me/Dropbox/Uni/Project/2-2/DKE-Project-2-2/GameInterop-master/src/main/java/Group9/map/maps/test_2.map"), new DefaultAgentFactory(), false);
         game.run();
         System.out.printf("The winner is: %s\n", game.getWinner());
 
--- GameInterop-master/src/main/java/Group9/gui2/Gui.java
+++ GameInterop-master/src/main/java/Group9/gui2/Gui.java
@@ -11,7 +11,7 @@ import java.io.File;
 import java.util.List;
 
 public class Gui extends Application {
-    private File mapFile = new File("./src/main/java/Group9/map/maps/test_2.map");
+    private File mapFile = new File("C:/Users/Me/Dropbox/Uni/Project/2-2/DKE-Project-2-2/GameInterop-master/src/main/java/Group9/map/maps/test_2.map");
     private MainController mainController = new MainController(this,mapFile,true);
     private MainScene scene = new MainScene(new StackPane(), mainController.getGame().getGameMap(),this);
     private Stage primary = new Stage();
